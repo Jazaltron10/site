79a9b8a178db5214d8bd745cd96713b8aa6a6d33
Jazaltron <jazaltron10@gmail.com>
2024-08-16 13:57:37 +0100

    Update .gitignore

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product, ProductDetail
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'product_list': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    details = get_object_or_404(ProductDetail, product=product)
+    return render(request, 'products_detail.html', {'product': product, 'details': details})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
