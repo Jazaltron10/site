3118e416682c45f1fe416cebc47bde3157cf59d9
Jazaltron <jazaltron10@gmail.com>
2024-08-20 12:19:43 +0100

    Site 4 - try-001

--- mysite/core/admin.py
+++ mysite/core/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin  # Import Django's admin interface
+from .models import Company, Department, Employee  # Import the models
 
-# Register your models here.
+admin.site.register(Company)  # Register the Company model with the admin site
+admin.site.register(Department)  # Register the Department model with the admin site
+admin.site.register(Employee)  # Register the Employee model with the admin site
